54e16ffcb233db84ad5229c7d5424b363f4239c2
ManuelGraziosi <manuel.graziosi@gmail.com>
2026-01-21 23:17:22 +0100

    feat group by query (wip)

--- query.sql
+++ query.sql
@@ -45,3 +45,19 @@ FROM departments
 SELECT COUNT(*)
 FROM teachers
 WHERE phone IS NULL
+
+-- GROUP BY
+
+-- 1. Contare quanti iscritti ci sono stati ogni anno
+SELECT YEAR(enrolment_date) AS "YEAR OF ENROLMENT", COUNT(id) AS "NUMBER OF STUDENTS"
+FROM students
+GROUP BY YEAR(enrolment_date)
+ORDER BY YEAR(enrolment_date)
+
+-- 2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
+
+
+-- 3. Calcolare la media dei voti di ogni appello d'esame
+
+
+-- 4. Contare quanti corsi di laurea ci sono per ogni dipartimento
